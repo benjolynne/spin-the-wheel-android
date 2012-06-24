67da9f012fbf58cc60b7993d65e4e56f28a1d2ce
Kelvin Clark Te <kelvin@do.not.email.this>
2012-06-24 12:23:34 +0800

    Initiated project startup.

---
+++ com.spin.the.wheel/src/com/spin/the/wheel/home.java
@@ -0,0 +1,14 @@
+package com.spin.the.wheel;
+
+import com.spin.the.wheel.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class home extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
